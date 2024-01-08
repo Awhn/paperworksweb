4d518904d17abc101e12fb9d1fd1e77749d5afa2
Awhn <ahnshwan@outlook.com>
2024-01-08 18:00:35 +0900

    Add document

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', main.site.urls),
+    path('games/', games.site.urls),
+    path('contact/', contact.site.urls),
+    path('signin/', signin.site.urls)
 ]
